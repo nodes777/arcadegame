a0f42878c49df1b566ab692fa83546c98238fc16
nodes777 <nodes777@gmail.com>
2015-11-24 22:09:00 -0500

    JS Lints

--- js/app.js
+++ js/app.js
@@ -40,16 +40,16 @@ var allEnemies = [ new Enemy(100, 50, 100),  new Enemy(200, 220, 10),  new Enemy
 // a handleInput() method.
 
 var Player = function(x, y) {
-    this.x = 200;
-    this.y = 400;
+    this.x = x;
+    this.y = y;
 
     this.sprite = 'images/char-boy.png';
 };
 
 
 Player.prototype.update = function(dt) {
-    this.x*dt;
-    this.y*dt;
+    this.x = this.x;
+    this.y = this.y;
    //this.checkEndGame(player);
 };
 Player.prototype.render = function() {
@@ -70,34 +70,34 @@ var reset = function (){
      alert("Congratulations! You win!");
             player.x = 200;
             player.y = 400;
-}
+};
 Player.prototype.handleInput = function(dir) {
     if( this.x < 350){ //contains player movement to canvas
         if(dir==='right') this.x+=100;
-    };
+    }
     if( this.x > 10){
         if(dir==='left') this.x-=100;
-    };
+    }
     if( this.y > 10){
         if(dir==='up') this.y-=83;
-    };
+    }
     if( this.y < 400){
         if(dir==='down') this.y+=83;
-    };
-    var rect1 = {x: 0, y: 0, width: 500, height: 20}
-    var rect2 = {x: this.x, y: this.y, width: 20, height: 20}
+    }
+    var rect1 = {x: 0, y: 0, width: 500, height: 20};
+    var rect2 = {x: this.x, y: this.y, width: 20, height: 20};
 
     if (rect1.x < rect2.x + rect2.width &&
        rect1.x + rect1.width > rect2.x &&
        rect1.y < rect2.y + rect2.height &&
        rect1.height + rect1.y > rect2.y) {
-          setTimeout(function(){reset()}, 500);
+          setTimeout(function(){reset();}, 500);
     }
 
 
-}
+};
 // Place the player object in a variable called player
-var player = new Player(20, 20);
+var player = new Player(200, 400);
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
